b8173fd7d2fb3c259cbb6fd9e595f71b00a64242
Mohamed Assem <mo7mad.assim@gmail.com>
2018-09-29 20:59:16 +0200

    Add permissions uitl

---
+++ app/src/main/java/com/assem/tests/iotblue/Utils/PermissionUtil.java
@@ -0,0 +1,34 @@
+package com.assem.tests.iotblue.Utils;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+public class PermissionUtil {
+
+    public boolean requestPermissions(Activity activity, List<String> permissions) {
+        final boolean[] flag = {false};
+        Dexter.withActivity(activity)
+                .withPermissions(permissions)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted())
+                            flag[0] = true;
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+        return flag[0];
+    }
+}
